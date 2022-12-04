eab96b09621a8c5b997a4403d3c8a33de60bd460
Sil Demmer <Sil@Hey.com>
2022-12-04 16:09:55 -0500

    pairAvailableStaff and comments

--- Schedule.py
+++ Schedule.py
@@ -30,12 +30,62 @@
 # select next role from the role collection...repeat until all roles from the role collection have been paired with a member of the staff collection.
 
 
-def formSchedule_stuck(roleCollection, staffColletion):
-	schedule = []
+def pairAvailableStaff(roleCollection, staffColletion):
+	roleStaffPairs = []
 	for role in roleCollection: # select the first role of the role collection.
 		availableStaff = [staff for staff in staffColletion if staff.isAvailable(role.callTime)] # from the staff collection, get a pool of all staff who are available for the selected role's call time.
+		if availableStaff == []:
+			raise RuntimeError(f'No staff available for {role}')
 		availableStaff.sort(key = shiftsRemaining) # order the pool of available staff with highest shifts remaining at the front.
 		staff = availableStaff[0] # select the first staff from the ordered pool.
+		roleStaffPairs.append((role,staff)) #pair selected staff with selected role.
+	return roleStaffPairs
+
+	
+
+		#now, the staff does not get paired when they meet one of the following exceptions.
+		#in that case, the next staff gets picked from the list.
+
+		#so, first we pick the first staff and check them against conditions of:
+		#Is the staff already paired with a role that same weekday?
+
+		#Another way to do it is to pair the role with the first staff of the available ordered list
+		#and then in a second pass, check each role and staff pair, and see if there are any doubles that day.
+		#When double is found, check for staff who is available and not scheduled that day.
+		#Replace the staff pairing.
+		#That is it's own sweep iteration.
+
+		#To do this, get a collection of all the roleStaffPairs for a certain weekday.
+		#To get this collection from the full week collection.
+		# in roleStaffCollection:
+		# find each pair from a certain weekday.
+
+		#weekdays come with roles, a role is of a weekday.
+		#when creating roleStaffPairs, the weekday seperation can be maintained.
+		#for weekday in Weekdays:
+			#for role in roleCollection.
+				#weekdayRoles = [role for role in roleCollection if role.day == 'weekday']
+
+
+		#For each multiple occurance of a staff in the roleStaffPairs.
+			#Find a replacement staff who is not scheduled for a shift that day.
+
+		#Then, once the double sweep has occured.
+		#An aptitdue sweep can take place.
+		# for each role/staff pair, is anyone paired into a role they are unsuited for?
+		#find replacements for staff pairings with roles.
+
+		#With each of these sweeps there are possible exceptions, how they are handeled, would be to the specfics of the sweep.
+		#This aligns with the modular setup I am wanting.
+
+
+		#Another way to phrase this is.
+		#Select first staff from the order pool, and check them against a set of conditions
+		#if all conditions are False, pair the selected staff with selected role.
+
+
+
+
 
 		#UNLESS following conditons are true. This is where I am stuck on how to write this.
 		# if selected staff is already scheduled for a shift on the selected role's weekday:
@@ -50,10 +100,7 @@ def formSchedule_stuck(roleCollection, staffColletion):
 		#There is some basic grasp of loops or if statement flow that I am apparently not understanding.
 
 
-		schedule.append((role,staff))
-
-
-def formSchedule(roleCollection, staffCollection):
+def formSchedule_00(roleCollection, staffCollection):
 	schedule = []
 	for role in roleCollection:
 		availableStaff = [staff for staff in staffCollection if staff.isAvailable(role.callTime)]
