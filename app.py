ec8a8f67cde569c74030ca888cd1117dcf969c69
Sil Demmer <Sil@Hey.com>
2023-01-03 15:45:58 -0500

    flask routes

--- app.py
+++ app.py
@@ -11,31 +11,30 @@ import main
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
+@app.route('/', methods=['GET','POST'])
+def login():
+    if request.method == 'GET':
+        return 'log in form'
+    if request.method == 'POST':
+        return 'do log in'
+
+@app.route('/availability/<staff>')
+def availability(staff):
+    return f'set {staff} availability'
+
+app.route('/createSchedule')
+def createSchedule():
+    return f'create schedule'
+
+@app.route("/showsense")
+def sense():
     name = request.args.get("name")
     preferredRole = request.args.get("role")
     return nonsense.nonsense(name=name, role=preferredRole)
 
-@app.route("/message")
-def html():
-    return 'some html text'
-
-@app.route("/dir/")
-def directory():
-    return 'directory?'
-
 @app.route('/profile/<username>')
 def profile(username):
     return f'{username}\'s profile'
-
-@app.get('/login')
-def login_get():
-    return 'get log in form'
-
-@app.post('/login')
-def login_post():
-    return 'post log in'
 
 @app.route('/hello/')
 @app.route('/hello/<name>')
@@ -43,7 +42,5 @@ def template(name=None):
     return render_template('hello.html', name=name)
 
 with app.test_request_context():
-    print(url_for('html'))
-    print(url_for('directory'))
-    #print(url_for('login'))
-    print(url_for('profile', username='TBody'))
+    print(url_for('login'))
+    print(url_for('availability', staff='TBody'))
