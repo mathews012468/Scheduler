416f4f4350e1046df58ab2de5372d5e32c89295f
Sil Demmer <Sil@Hey.com>
2023-01-03 13:41:16 -0500

    playing with Flask

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask import request
+from flask import url_for
+from flask import render_template
+
 import nonsense
 
 
 app = Flask(__name__)
 
-@app.route("/") #ah, so this is root of the url
+@app.route("/")
 def hello():
     name = request.args.get("name")
     preferredRole = request.args.get("role")
@@ -18,3 +21,26 @@ def html():
 @app.route("/dir/")
 def directory():
     return 'directory?'
+
+@app.route('/profile/<username>')
+def profile(username):
+    return f'{username}\'s profile'
+
+@app.get('/login')
+def login_get():
+    return 'get log in form'
+
+@app.post('/login')
+def login_post():
+    return 'post log in'
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def template(name=None):
+    return render_template('hello.html', name=name)
+
+with app.test_request_context():
+    print(url_for('html'))
+    print(url_for('directory'))
+    #print(url_for('login'))
+    print(url_for('profile', username='TBody'))
