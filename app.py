05ed956539af349e37aae8152b5d23f2c6907bb0
Sil Demmer <Sil@Hey.com>
2023-01-29 19:15:35 -0500

    removed some unused files and functions

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
 from flask import Flask
 from flask import request
-from flask import render_template
 import main
 import datetime
 
 from classes import Staff, Role, Weekdays
-import json
-
-#TODO: format availability from incoming json
 
 schema = {
     "roles": [
@@ -32,81 +28,9 @@ schema = {
 app = Flask(__name__)
 app.config['activeSchema'] = schema #TODO: config from seperate file
 
-@app.route('/query_example')
-def query_example():
-    language = request.args.get('language')
-    framework = request.args.get('framework')
-    website = request.args.get('website')
-
-    return f"""
-    <h1>The language value is: {language}</h1>
-    <p>The framework value is: {framework}</p>
-    The website value is: {website}
-    """
-
-@app.route('/form_example', methods=['GET','POST'])
-def form_example():
-    if request.method == 'POST':
-        language = request.form.get('language')
-        framework = request.form.get('framework')
-        return f'''
-        <p>The language value is: {language}</p>
-        The framework value is: {framework}'''
-    #else handle the 'GET' request
-    #return html to display a form which POSTS on submit
-    return """
-    <form method='POST'>
-    <div><label>Langue: <input type='text' name='language'></label</div>
-    <div><label>Framework: <input type'text' name='framework'</label></div>
-    <input type='submit' value='Submit'>
-    </form>"""
-
-
-@app.route('/json_example', methods=['POST'])
-def json_example():
-    requestData = request.get_json()
-
-    #It's like .get() without .get()
-    language = None
-    framework = None
-    pythonVersion = None
-    example = None
-    booleanTest = None
-
-    if requestData:
-        if 'language' in requestData:
-            language = requestData['language']
-        if 'framework' in requestData:
-            framework = requestData['framework']
-        if 'version_info' in requestData:
-            pythonVersion = requestData['version_info']['python']
-        if 'examples' in requestData:
-            example = requestData['examples'][0]
-        if 'boolean_test' in requestData:
-            booleanTest = requestData['boolean_test']
-
-    return f'''
-    The language value is: {language}
-    The framework value is: {framework}
-    The Python version is: {pythonVersion}
-    The item at index 0 of "examples" is: {example}
-    The boolean test value is: {booleanTest}
-    '''
-
 @app.route('/')
 def traditions():
     return 'hello world'
-
-        
-@app.route('/availability/<staff>')
-def availability(staff):
-    return f'set {staff} availability'
-
-
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def template(name=None):
-    return render_template('hello.html', name=name)
 
 def getAllCallTimes(roles):
     """
--- nonsense.py
+++
@@ -1,2 +0,0 @@
-def nonsense(name, role):
-    return f"The nonsense function was called with name {name} and role {role}"
