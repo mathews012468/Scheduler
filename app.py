da3bb1c4af3154a26383428c3e1e59d39c0576f0
Sil Demmer <Sil@Hey.com>
2023-02-05 20:28:50 -0500

    function ordering

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ import datetime
 
 from classes import Staff, Role, Weekdays
 
+#TODO: defaultAvail = getAllCallTimes(roles)
+    #decide where to have this happen, on the Appscript side.
+#TODO: config from seperate file
+
+
 schema = {
     "roles": [
         {
@@ -52,107 +57,20 @@ schema = {
 }
 
 app = Flask(__name__)
-app.config['roleStaffSchema'] = schema #TODO: config from seperate file
+app.config['roleStaffSchema'] = schema
+#I'd like to follow the documentation for seperating the config settings:
+# https://flask.palletsprojects.com/en/2.2.x/config/
+
+#However, I don't really understand it
+#TODO: write out current understanding and where I'm stuck.
+
+#app.config.from_object('kikischeduler.config')
+#app.config.from_envvar('KIKISCHEDULER_SETTINGS')
 
 @app.route('/')
 def traditions():
     return 'hello world'
 
-def getAllCallTimes(roles):
-    """
-    List of Role objects
-    """
-    dayAndCallTimes = {(role.day, role.callTime) for role in roles}
-    callTimes = {weekday: [] for weekday in Weekdays}
-    for dayCallTime in dayAndCallTimes:
-        weekday = dayCallTime[0]
-        callTime = dayCallTime[1]
-        callTimes[weekday].append(callTime)
-    return callTimes
-
-def parseRole(role):
-    """
-    Takes in role as dictionary from google sheets app script
-    Returns Role object
-    """
-    name = role["name"]
-    try:
-        hour, minutes = role["callTime"].split(":")
-        hour, minutes = int(hour), int(minutes)
-        callTime = datetime.time(hour=hour, minute=minutes)
-    except ValueError:
-        raise ValueError(f"Call time {role['callTime']} not in a valid format")
-    qualifiedStaff = role["qualifiedStaff"]
-    try:
-        day = role["day"]
-        weekday = Weekdays[day]
-    except KeyError:
-        raise ValueError(f"Day: {day} for Role: {name} not in valid format.")
-    return Role(name=name, day=weekday, callTime=callTime, qualifiedStaff=qualifiedStaff)
-
-def parseStaff(staff):
-    name = staff["name"]
-    maxShifts = staff["maxShifts"]
-    try:
-        availability = formatAvailability(staff["availability"])
-    except ValueError:
-        raise ValueError(f"Invalid format for Staff: {name} availability.")
-    rolePreference = staff["rolePreference"]
-    doubles = staff["doubles"]
-    return Staff(name=name, maxShifts=maxShifts, availability=availability, rolePreference=rolePreference, doubles=doubles)
-
-@app.route('/schedule', methods=["POST"])
-def compileStaff():
-    requestData = request.get_json()
-    if requestData == None:
-        return 'Alert: Check payload header'
-    validatePayload(requestData)
-    roleCollection = (parseRole(role) for role in requestData["roles"])
-
-    #TODO: defaultAvail = getAllCallTimes(roles)
-    staffCollection = [parseStaff(staff) for staff in requestData["staff"]]
-
-    schedule = main.createSchedule(roleCollection, staffCollection)
-    return schedule
-
-@app.route('/input', methods=['POST'])
-def validateResponse():
-    requestData = request.get_json()
-    if requestData == None:
-        return 'Alert: Check payload header'
-    isValid = validatePayload(requestData, app.config["roleStaffSchema"])
-    if not isValid:
-        return {"error": 'not valid input'}, 400
-    roleCollection = [parseRole(role) for role in requestData['roles']]
-    staffCollection = [parseStaff(staff) for staff in requestData['staff']]
-
-    schedule = main.createSchedule(roleCollection, staffCollection)
-
-    scheduleAsJson = scheduleToJSON(schedule)
-    
-    return scheduleAsJson
-
-def scheduleToJSON(schedule):
-    scheduleJSON = []
-    for pair in schedule:
-        role = pair[0]
-        staff = pair[1]
-        role.qualifiedStaff = [] # size down the character count for testing.
-        role.staff = staff.name
-        scheduleJSON.append(role.toJSON())
-    
-    for obj in scheduleJSON: #this does not seem to work here.
-        obj.replace('\\"','') #while it works in the python interpreter to clean the dumped escape characters
-
-    return scheduleJSON
-
-def printWeekSchedule(schedule):
-	for day in Weekdays:
-		print(day.name)
-		dayPairs = [pair for pair in schedule if pair[0].day == day]
-		for pair in dayPairs:
-			print(pair[0].name, pair[1].name)
-    
 #if dict, check that keys are the same and schema of values are the same
 #if list, check that schema of values are the same
 #if plain value, check that type is the same
@@ -179,6 +97,40 @@ def validatePayload(payload, schema):
     
     return isValid
 
+
+def parseRole(role):
+    """
+    Takes in role as dictionary from google sheets app script
+    Returns Role object
+    """
+    name = role["name"]
+    try:
+        hour, minutes = role["callTime"].split(":")
+        hour, minutes = int(hour), int(minutes)
+        callTime = datetime.time(hour=hour, minute=minutes)
+    except ValueError:
+        raise ValueError(f"Call time {role['callTime']} not in a valid format")
+    qualifiedStaff = role["qualifiedStaff"]
+    try:
+        day = role["day"]
+        weekday = Weekdays[day]
+    except KeyError:
+        raise ValueError(f"Day: {day} for Role: {name} not in valid format.")
+    return Role(name=name, day=weekday, callTime=callTime, qualifiedStaff=qualifiedStaff)
+
+
+def parseStaff(staff):
+    name = staff["name"]
+    maxShifts = staff["maxShifts"]
+    try:
+        availability = formatAvailability(staff["availability"])
+    except ValueError:
+        raise ValueError(f"Invalid format for Staff: {name} availability.")
+    rolePreference = staff["rolePreference"]
+    doubles = staff["doubles"]
+    return Staff(name=name, maxShifts=maxShifts, availability=availability, rolePreference=rolePreference, doubles=doubles)
+
+
 def formatAvailability(availability):
     staffAvailability = {}
     for day, callTimes in availability.items():
@@ -194,3 +146,37 @@ def formatAvailability(availability):
             weekdayCalltimes.append(callTime)
         staffAvailability[weekday] = weekdayCalltimes
     return staffAvailability
+
+
+def scheduleToJSON(schedule):
+    scheduleJSON = []
+    for pair in schedule:
+        role = pair[0]
+        staff = pair[1]
+        role.qualifiedStaff = [] # size down the character count for testing.
+        role.staff = staff.name
+        scheduleJSON.append(role.toJSON())
+    
+    for obj in scheduleJSON: #this does not seem to work here.
+        obj.replace('\\"','') #while it works in the python interpreter to clean the dumped escape characters
+
+    return scheduleJSON
+
+
+@app.route('/schedule', methods=["POST"])
+def createSchedule():
+    roleStaffData = request.get_json()
+    roleStaffSchema = app.config["roleStaffSchema"]
+    if roleStaffData == None:
+        return 'Alert: Check payload header'
+    isValid = validatePayload(roleStaffData,roleStaffSchema)
+    if not isValid:
+        return {"error": 'invalid input'}, 400
+
+    roleCollection = (parseRole(role) for role in roleStaffData["roles"])
+    staffCollection = [parseStaff(staff) for staff in roleStaffData["staff"]]
+
+    schedule = main.createSchedule(roleCollection, staffCollection)
+    scheduleJSON = scheduleToJSON(schedule)
+
+    return scheduleJSON
