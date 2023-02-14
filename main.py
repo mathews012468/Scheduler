389f9ff8f787a418a5512202e84615fc1c706f5d
Sil Demmer <Sil@Hey.com>
2023-02-14 16:27:03 -0500

    createSchedule rewriting

--- main.py
+++ main.py
@@ -10,24 +10,16 @@ def staffWorkingToday(roleStaffPairs, weekday):
 
 
 def createSchedule(roleCollection, staffCollection):
-	"""
-	returns a 'schedule as a list of (role,staff) pair tuples
-	Assumes Role and Staff objects have been compiled from spreadsheet database
-	"""
-	roleCollection.sort(key=lambda role: len(role.qualifiedStaff))
-	roleStaffPairs = []
-	for role in roleCollection:
-		availableStaff = [staff for staff in staffCollection if staff.name in role.qualifiedStaff and staff.isAvailable(role) and staff not in staffWorkingToday(roleStaffPairs, role.day)] # I don't like how long this line is.
-		#availableStaff = [staff for staff in availableStaff if role.name in staff.rolePreference]
-		availableStaff = [staff for staff in availableStaff if staff.shiftsRemaining(roleStaffPairs) > 0]
-		if availableStaff == []:
-			unassigned = Staff(name='Unassigned',maxShifts=None, availability=None)
-			roleStaffPairs.append((role,unassigned))
-			continue
-		availableStaff.sort(key = lambda staff: staff.shiftsRemaining(roleStaffPairs), reverse=True)
-		staff = availableStaff[0]
-		roleStaffPairs.append((role,staff))
-	return roleStaffPairs
+    roleStaffPairs = []
+    for role in roleCollection:
+        availableStaff = [staff for staff in staffCollection if staff.isAvailable(role) and staff.isQualified(role)]
+        preferedStaff = [staff for staff in availableStaff if role.name in staff.rolePreference]
+        if preferedStaff != []:
+            availableStaff = preferedStaff
+        availableStaff.sort(key= lambda staff: staff.shiftsRemaining(roleStaffPairs), reverse= True)
+        staff = availableStaff[0]
+        roleStaffPairs.append((role,staff))
+    return roleStaffPairs
 
 
 def validatePayload(payload, schema):
