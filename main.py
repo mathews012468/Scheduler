e3b53335f2fd76eb3ede4e908ec040359b139f20
Sil Demmer <Sil@Hey.com>
2023-02-22 16:13:39 -0500

    removed log/test sketch functions

--- main.py
+++ main.py
@@ -38,26 +38,6 @@ def selectStaff(staffPool, schedule, role):
     raise IndexError('No staff found')
     
 
-#is this a logging or testing functionality?
-def logSchedule(schedule):
-    with open('output', 'w') as f:
-        doubleOutput = log_hasDoubles(schedule)
-        f.write(doubleOutput)
-
-def log_hasDoubles(schedule):
-    #return 'yes' or 'no'
-    dayStaffPairs = []
-    for pair in schedule:
-        day = pair[0].day
-        staff = pair[1]
-        if (day,staff) in dayStaffPairs:
-            return 'yes'
-        else:
-            dayStaffPairs.append((day,staff))
-    return 'no'
-
-
-
 def validatePayload(payload, schema):
     """ Takes in payload and checks key/value pairs against a schema """
     payloadType, schemaType = type(payload), type(schema)
