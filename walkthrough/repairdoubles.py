6ea109b26dba67be33fa5bebc75f04794f2e9f9b
Sil Demmer <Sil@Hey.com>
2023-03-02 14:53:58 -0500

    start of repairDoubles for schedule iteration

---
+++ walkthrough/repairdoubles.py
@@ -0,0 +1,17 @@
+def repairDouble(schedule, double):
+    role = double[0]
+    assignedStaff=double[1]
+
+    #for possible swaps
+    #find all staff not working on role.day
+        #those possible people to swap with
+
+    #for assigned staff
+    #find all the days that assigned is not working
+
+    #find all roles where assignedstaff is not working on role.day
+    #and where role in question is on a day that assigned staff is not working
+
+    #select random
+    #make swap
+
--- walkthrough/scheduleIterations.py
+++ walkthrough/scheduleIterations.py
@@ -1,12 +1,57 @@
 import random
+import logging
+
+logger = logging.getLogger(__name__)
 
 def startSchedule(roleCollection, staffCollection):
+    """
+    For each role, pick random employee with max shifts remaining
+    """
+    #generate dictionary where keys are shifts remaining
+    #and values are list of employees with that number of shifts
+    staffByShifts = {}
+    for staff in staffCollection:
+        shiftsRemaining = staff.maxShifts
+        staffByShifts.setdefault(shiftsRemaining, [])
+        staffByShifts[shiftsRemaining].append(staff)
+    maxRemainingShifts = max(staffByShifts)
+
+    #shuffle role collection
+    roles = random.sample(roleCollection, k=len(roleCollection))
     schedule = []
-    for role in roleCollection:
-        staff = random.choice(staffCollection)
-        schedule.appened((role,staff))
+    for role in roles:
+        staff = random.choice(staffByShifts[maxRemainingShifts])
+        #move staff to lower key, possibly remove key and update max
+        #if that was the last staff with that number of shifts remaining
+        staffByShifts[maxRemainingShifts].remove(staff)
+        staffByShifts.setdefault(maxRemainingShifts-1, [])
+        staffByShifts[maxRemainingShifts-1].append(staff)
+        if staffByShifts[maxRemainingShifts] == []:
+            del staffByShifts[maxRemainingShifts]
+            maxRemainingShifts -= 1
+        schedule.append((role,staff))
+    logger.debug(staffByShifts)
     return schedule
 
+def identifyDoubles(roleStaffPairs):
+	"""
+	take in current schedule, list of (role, staff)
+	return list of indices of staff that are doubled in a day in that list
+	"""
+	
+	#if staff has already worked that day, then it's a double
+	doubleIndices = []
+	staffDays = set() #set of staff day pairs
+	for index, pair in enumerate(roleStaffPairs):
+		staff = pair[1]
+		day = pair[0].day
+		staffDay = (staff, day)
+
+		if staffDay in staffDays:
+			doubleIndices.append(index)
+		else:
+			staffDays.add(staffDay)
+	return doubleIndices
 
 def createSchedule(roleCollection, staffCollection):
     schedule = startSchedule(roleCollection, staffCollection)
@@ -14,16 +59,19 @@ def createSchedule(roleCollection, staffCollection):
     schedule = repairMaxShifts(schedule)
     schedule = repairAvailability(schedule)
     schedule = repairPreferences(schedule)
+
+    doublesIndices = identifyDoubles(schedule)
+    logger.debug([schedule[index] for index in doublesIndices])
     return schedule
 
-def repairDoubles():
-    pass
+def repairDoubles(schedule):
+    return schedule
 
-def repairMaxShifts():
-    pass
+def repairMaxShifts(schedule):
+    return schedule
 
-def repairAvailability():
-    pass
+def repairAvailability(schedule):
+    return schedule
 
-def repairPreferences():
-    pass
+def repairPreferences(schedule):
+    return schedule
