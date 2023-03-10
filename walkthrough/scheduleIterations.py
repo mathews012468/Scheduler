388c5d16e02926dcf9c3f0eb58fca5560c804c0f
mathews012468 <Mathew.Soto78@myhunter.cuny.edu>
2023-03-10 09:34:57 -0500

    finding all cycles of arbitrary length

--- walkthrough/scheduleIterations.py
+++ walkthrough/scheduleIterations.py
@@ -1,6 +1,7 @@
 import random
 import logging
 from classes import Weekdays
+import copy
 
 logger = logging.getLogger(__name__)
 
@@ -137,73 +138,85 @@ def identifyDoubles(roleStaffPairs):
 			staffDays.add(staffDay)
 	return doubles
 
+def couldWorkRole(staff, role, schedule):
+    allDays = {day for day in Weekdays}
+    staffWorkingDays = {pair[0].day for pair in schedule if pair[1] is staff}
+    possibleSwapDays = allDays - staffWorkingDays
+    staffAlreadyWorksRole = False
+    for pair in schedule:
+        if pair[1] == staff and pair[0] == role:
+            staffAlreadyWorksRole = True
+            break
+
+    return (role.day in possibleSwapDays or staffAlreadyWorksRole) and staff.isAvailable(role)
+
 def repairUnavailables(schedule):
-    unavailables = identifyUnavailables(schedule)
-    logger.debug(f"Unavailables to fix: {unavailables}")
-    logger.debug(f"Number of unavailables: {len(unavailables)}")
-
-    MAX_ATTEMPTS = 100
+    graph = [[couldWorkRole(pair1[1], pair2[0], schedule) for pair1 in schedule] for pair2 in schedule]
+    logger.debug(f"graph: {graph}")
+    unavailables = [i for i in range(len(graph)) if not graph[i][i]]
+    
+    MAX_ATTEMPTS = 1
     attempts = 0
-
     while unavailables != [] and attempts < MAX_ATTEMPTS:
-        unavailableToRepair = random.choice(unavailables)
-        schedule = repairUnavailable(schedule, unavailableToRepair)
-        unavailables = identifyUnavailables(schedule)
+        indexOfUnavailableToRepair = random.choice(unavailables)
+        schedule = repairUnavailable(schedule, indexOfUnavailableToRepair)
+        graph = [[couldWorkRole(pair1[1], pair2[0], schedule) for pair1 in schedule] for pair2 in schedule]
+        unavailables = [i for i in range(len(graph)) if not graph[i][i]]
 
         attempts += 1
 
+    logger.debug(f"Unavailables: {unavailables}")
     return schedule
 
-def repairUnavailable(schedule, unavailableToRepair):
-    logger.info(f"Unavailable pair to be resolved: {unavailableToRepair}")
-    unavailableRole = unavailableToRepair[0]
-    unavailableStaff = unavailableToRepair[1]
+def repairUnavailable(schedule, indexOfUnavailableToRepair):
+    graph = [[1 if couldWorkRole(pair1[1], pair2[0], schedule) else 0 for pair1 in schedule] for pair2 in schedule]
+    length = 2
+    path = [indexOfUnavailableToRepair]
+    visited = [False for i in range(len(schedule))]
+    visited[indexOfUnavailableToRepair] = True
+    allCycles = allCyclesOfLength(graph, length, path, visited)
+    logger.debug(f"allCycles: {allCycles}")
+    return schedule
 
-    #find all staff not working on day of unavailability 
-    # and are available for the role to be repaired
-    allStaff = {pair[1] for pair in schedule}
-    staffWorkingThatDay = {pair[1] for pair in schedule if pair[0].day is unavailableRole.day}
-    staffAvailableForRole = {pair[1] for pair in schedule if pair[1].isAvailable(unavailableRole)}
-    possibleSwapPartners = (allStaff - staffWorkingThatDay) & staffAvailableForRole
+def allCyclesOfLength(graph, length, path, visited):
+    """
+    Find all paths of length 'length' in 'graph' building off of path 
+    and ending at the start of path (which makes a cycle). 
+    Graph is a list of lists, is an adjacency matrix. 
+    Path is a list of the elements in the path so far (list[int])
+    Length is an int representing how many more nodes we need to walk along in the path
+    visited is a list of bools letting us know which nodes have been visited (so we don't visit them again)
 
-    #find all days where unavailable staff isn't working
-    allDays = {day for day in Weekdays}
-    daysWhenUnavailableStaffIsWorking = {pair[0].day for pair in schedule if pair[1] is unavailableStaff}
-    possibleSwapDays = allDays - daysWhenUnavailableStaffIsWorking
-
-    #list of roles unavailable staff could work that are currently being staffed
-    #by one of the possible swap partners
-    possibleSwapPairs = [pair for pair in schedule if pair[0].day in possibleSwapDays and unavailableStaff.isAvailable(pair[0]) and pair[1] in possibleSwapPartners]
-    logger.debug(f"Possible swap pairs (availability): {possibleSwapPairs}")
-
-    if possibleSwapPairs == []:
-        #no way to resolve this unavailability, so just return the schedule as is
-        logger.info(f"{unavailableToRepair} cannot be resolved at the moment.")
-        return schedule
+    Return a list[list[int]]
+    """
+    cycles = []
+    currentNode = path[-1]
+    if length == 1:
+        startNode = path[0]
+        #only add path to cycles if the current node connects to the start node
+        if graph[currentNode][startNode]:
+            path = path + [startNode]
+            cycles.append(path)
+        return cycles
     
-    #pick a random role from that list as the swap
-    swapPair = random.choice(possibleSwapPairs)
+    unvisitedNeighbors = [index for index, (isNeighbor, didVisit) in enumerate(zip(graph[currentNode], visited)) if isNeighbor and not didVisit]
+    logger.debug(f"length: {length}, path: {path}, visited: {visited}, unvisitedNeighbors: {unvisitedNeighbors}")
+    for neighbor in unvisitedNeighbors:
+        newVisited = copy.deepcopy(visited)
+        newVisited[neighbor] = True
+        newCycles = allCyclesOfLength(graph, length-1, path + [neighbor], newVisited)
+        logger.debug(f"neighbor: {neighbor}, newCycles: {newCycles}")
+        cycles.extend(newCycles)
+    return cycles
 
-    #to make swap
-    #1. remove pairs to be changed
-    schedule.remove(unavailableToRepair)
-    schedule.remove(swapPair)
-    #2. swap staff
-    newPair1 = (unavailableRole, swapPair[1])
-    newPair2 = (swapPair[0], unavailableStaff)
-    #3. add fixed pairs back to schedule
-    schedule.append(newPair1)
-    schedule.append(newPair2)
-
-    logger.info(f"Unavailability resolved: {unavailableToRepair}")
-    return schedule
+             
 
 def identifyUnavailables(schedule):
     """
     Return list of all role-staff pairs where the staff is
     unavailable to work the role
     """
-    return [pair for pair in schedule if not pair[1].isAvailable(pair[0])]
+    return [i for i, pair in enumerate(schedule) if not pair[1].isAvailable(pair[0])]
 
 def repairPreferences(schedule):
     return schedule
