0ba64ff340096c3ba0ee3714bb5eac2cbc1c0027
Sil Demmer <Sil@Hey.com>
2023-03-30 20:06:06 -0400

    code import: Schedule as class

--- scheduleIterations.py
+++ scheduleIterations.py
@@ -4,35 +4,43 @@ from classes import Weekdays
 
 logger = logging.getLogger(__name__)
 
-def startSchedule(roleCollection, staffCollection):
-    """
-    For each role, pick random employee with max shifts remaining
-    """
-    #generate dictionary where keys are shifts remaining
-    #and values are list of employees with that number of shifts
-    staffByShifts = {}
-    for staff in staffCollection:
-        shiftsRemaining = staff.maxShifts
-        staffByShifts.setdefault(shiftsRemaining, [])
-        staffByShifts[shiftsRemaining].append(staff)
-    maxRemainingShifts = max(staffByShifts)
+class Schedule:
+    def __init__(self, roles, staff):
+        self.roles = roles
+        self.staff = staff
+        self.schedule = self.startSchedule()
+    
+    def startSchedule(self):
+        """
+        For each role, pick random employee with max shifts remaining
+        """
+        #generate dictionary where keys are shifts remaining
+        #and values are list of employees with that number of shifts
+        staffByShifts = {}
+        for staff in self.staff:
+            #makes sure shifts remaining aligns with a staff's indicated availability
+            shiftsRemaining = min(staff.maxShifts, numberOfDaysCouldWork(staff))
+            logger.debug(f"Staff: {staff}, shifts remaining: {shiftsRemaining}, max shifts: {staff.maxShifts}, days could work: {numberOfDaysCouldWork(staff)}")
+            staffByShifts.setdefault(shiftsRemaining, [])
+            staffByShifts[shiftsRemaining].append(staff)
+        maxRemainingShifts = max(staffByShifts)
 
-    #shuffle role collection
-    roles = random.sample(roleCollection, k=len(roleCollection))
-    schedule = []
-    for role in roles:
-        staff = random.choice(staffByShifts[maxRemainingShifts])
-        #move staff to lower key, possibly remove key and update max
-        #if that was the last staff with that number of shifts remaining
-        staffByShifts[maxRemainingShifts].remove(staff)
-        staffByShifts.setdefault(maxRemainingShifts-1, [])
-        staffByShifts[maxRemainingShifts-1].append(staff)
-        if staffByShifts[maxRemainingShifts] == []:
-            del staffByShifts[maxRemainingShifts]
-            maxRemainingShifts -= 1
-        schedule.append((role,staff))
-    logger.debug(staffByShifts)
-    return schedule
+        #shuffle role collection
+        roles = random.sample(self.roles, k=len(self.roles))
+        schedule = {}
+        for role in roles:
+            staff = random.choice(staffByShifts[maxRemainingShifts])
+            #move staff to lower key, possibly remove key and update max
+            #if that was the last staff with that number of shifts remaining
+            staffByShifts[maxRemainingShifts].remove(staff)
+            staffByShifts.setdefault(maxRemainingShifts-1, [])
+            staffByShifts[maxRemainingShifts-1].append(staff)
+            if staffByShifts[maxRemainingShifts] == []:
+                del staffByShifts[maxRemainingShifts]
+                maxRemainingShifts -= 1
+            schedule[role] = staff
+        logger.debug(staffByShifts)
+        return schedule
 
 def createSchedule(roleCollection, staffCollection):
     schedule = startSchedule(roleCollection, staffCollection)
